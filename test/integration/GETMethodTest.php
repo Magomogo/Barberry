4ef1e18e1e2def58416daaec9026325f580b58fd
Magomogo <mgnatenko@gmail.com>
2012-03-06 13:04:35 +0700

    * More verbose test error message

--- test/integration/GETMethodTest.php
+++ test/integration/GETMethodTest.php
@@ -8,8 +8,10 @@ class GETMethodTest extends PHPUnit_Framework_TestCase {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $error = curl_error($ch);
         curl_close($ch);
 
-        $this->assertEquals(404, $httpCode);
+        $this->assertEquals(404, $httpCode, $error);
     }
 }
+
