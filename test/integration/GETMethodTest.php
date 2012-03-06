151d27fcae30bf0a8baa785ae5cbe61fd9c01442
Magomogo <mgnatenko@gmail.com>
2012-03-06 13:02:22 +0700

    + Configuration that can be overrided

---
+++ test/integration/GETMethodTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class GETMethodTest extends PHPUnit_Framework_TestCase {
+
+    public function testEmptyGetReturns404Error() {
+        $ch = curl_init('http://' . Config::get()->httpHost);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $this->assertEquals(404, $httpCode);
+    }
+}
---
+++ test/unit/ConfigTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class ConfigTest extends PHPUnit_Framework_TestCase {
+
+    public function testConfigIsSingleton() {
+        $this->assertSame(
+            Config::get(),
+            Config::get()
+        );
+    }
+
+    public function testOptionsCanBeOverriden() {
+        $config = new Config(array('httpHost' => 'bin.myhost.ch'));
+        $this->assertEquals('bin.myhost.ch', $config->httpHost);
+    }
+}
