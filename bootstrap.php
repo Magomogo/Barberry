10b0ac7db603a5a280c698fea12217acd96e14bd
Magomogo <mgnatenko@gmail.com>
2012-03-12 11:38:50 +0700

    + Strict standarts enabled + Check for required php extensions

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
 define('APPLICATION_PATH', __DIR__);
 
 # convert errors into exceptions
--- scripts/install/check_dependencies.php
+++ scripts/install/check_dependencies.php
@@ -1,9 +1,54 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
 
-if (!version_compare(PHP_VERSION, '5.3.0', '>=')) {
-    die ("PHP 5.3.0 or later required\n");
-} else {
-    echo "PHP " . PHP_VERSION . ": OK";
+echo "\nChecking pre-requisites required to install, develop, and run the application:\n\n";
+
+reportVersion('PHP', '5.3.0', PHP_VERSION);
+
+$can_proceed = true;
+foreach (
+    array(
+        'fileinfo',
+        'curl'
+    ) as $requiredPhpExtension) {
+    $can_proceed &= reportPhpExtension($requiredPhpExtension);
+}
+
+if (!$can_proceed) {
+    echo "Please install missed PHP extenstions.\n\n";
+    exit;
 }
 
 echo "\nAll critical checks passed SUCCESSFULLY\n\n";
+
+//==================================================================================================
+
+function reportVersion($componentName, $requiredVersion, $currentVersion, $whiteList = array()) {
+    echo "$componentName version: $currentVersion - ";
+
+    if(!empty($whiteList) && in_array($currentVersion, $whiteList)) {
+        echo "OK\n";
+    }
+    else if (version_compare($requiredVersion, $currentVersion) <= 0) {
+        echo "OK\n";
+    }
+    else {
+        echo "INSUFFICIENT - $requiredVersion required\n\n";
+        exit;
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+function reportPhpExtension($name) {
+    echo "$name module - ";
+
+    if (extension_loaded($name)) {
+        echo "LOADED\n";
+        return true;
+    }
+    else {
+         echo "MISSING\n";
+         return false;
+    }
+}
