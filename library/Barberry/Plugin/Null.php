e35412e4c1c313877d832949d9dda5f3862b039f
Magomogo <mgnatenko@gmail.com>
2012-11-07 11:49:03 +0700

    * Make Null plugin compatible to interfaces 0.2

--- library/Barberry/Plugin/Null.php
+++ library/Barberry/Plugin/Null.php
@@ -1,9 +1,20 @@
 <?php
 namespace Barberry\Plugin;
+use Barberry\ContentType;
 
 class Null implements InterfaceConverter {
 
     public function convert($bin, InterfaceCommand $command = null) {
         return $bin;
+    }
+
+    /**
+     * @param ContentType $targetContentType
+     * @param string $tempPath
+     * @return self
+     */
+    public function configure(ContentType $targetContentType, $tempPath)
+    {
+        return $this;
     }
 }
