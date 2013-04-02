4330002500ef6af5e33513a2cc00cd7f8e315bcc
Magomogo <mgnatenko@gmail.com>
2013-04-02 10:41:32 +0700

    Fixed test bootstrapping

--- test/test_bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-include __DIR__ . '/../vendor/autoload.php';
-
-require_once 'Mockery/Loader.php';
-require_once 'Hamcrest/Hamcrest.php';
-$loader = new \Mockery\Loader;
-$loader->register();
