1b72e76e8b0e3f49aa458924c41858fa24b8bab2
Sergey Zhdankin <sergey.zhdankin@xiag.ch>
2016-02-17 10:59:13 +0600

    Added checking for extension openssl are loaded, else using uniqid

--- library/Barberry/Uniq.php
+++ library/Barberry/Uniq.php
@@ -10,7 +10,10 @@ class Uniq
 {
     public static function id($len = 10)
     {
-        $bytes = openssl_random_pseudo_bytes($len);
-        return bin2hex($bytes);
+        if (extension_loaded('openssl')) {
+            $bytes = openssl_random_pseudo_bytes($len);
+            return bin2hex($bytes);
+        }
+        return $len > 10 ? md5(uniqid('', true)) : uniqid('');
     }
 }
